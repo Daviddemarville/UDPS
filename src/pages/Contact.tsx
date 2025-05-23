dda59bd3f7f679d6f915f3e300e0f0f9490d5799
Daviddemarville <de.marville.david@gmail.com>
2025-05-23 17:53:37 +0200

    add fix background + legal.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,12 @@
 export default function Contact() {
 	return (
-		<div>
-			<h1>Contact</h1>
+		<div className="text-center">
+			<h1 className=" text-2xl font-extrabold mb-15">Contact</h1>
+			<h1 className=" text-red-500 text-2xl font-extrabold mb-15">
+				Notre site est en cours de mise en ligne. Certaines fonctionnalités,
+				comme ce formulaire, peuvent ne pas encore fonctionner correctement.
+				Merci pour votre patience et votre compréhension !
+			</h1>
 		</div>
 	);
 }
--- src/pages/Don.tsx
+++ src/pages/Don.tsx
@@ -1,7 +1,12 @@
 export default function Don() {
 	return (
-		<div>
-			<h1>Faire un don</h1>
+		<div className="text-center">
+			<h1 className=" text-2xl font-extrabold mb-15">Faire un don</h1>
+			<h1 className=" text-red-500 text-2xl font-extrabold mb-15">
+				Notre site est en cours de mise en ligne. Certaines fonctionnalités,
+				peuvent ne pas encore fonctionner correctement. Merci pour votre
+				patience et votre compréhension !
+			</h1>
 		</div>
 	);
 }
